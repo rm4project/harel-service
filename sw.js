2f740d1618ba15edc04d3ccf59ccdda1d70ce3a6
manzuraniRani <manzurani@gmail.com>
2026-04-21 12:22:25 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'harel-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/morot.html','/tech.html','/maint.html','/admin.html'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
